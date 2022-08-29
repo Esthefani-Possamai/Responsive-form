7466862ab353b833f1e1ad55bd87b1702d13c11e
lisfe00 <luisfelipebelle7@gmail.com>
2022-08-29 13:27:56 -0300

    esconder imput outros

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,52 @@
-
 //MASCARA PARA CPF E CNPJ NO MESMO CAMPO
 
-function mascara_cpf_cnpj(){
-    var cpf_cnpj = document.querySelector('#cpf_cnpj')
-    var ncpf = cpf_cnpj.value.length;
+function mascara_cpf_cnpj() {
+  var cpf_cnpj = document.querySelector("#cpf_cnpj");
+  var ncpf = cpf_cnpj.value.length;
 
-    if(ncpf <= 11){
-
-        cpf_cnpj.value = cpf_cnpj.value.replace(/^(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
-
-    }else if(ncpf > 11){
-
-        cpf_cnpj.value = cpf_cnpj.value.replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, "$1.$2.$3/$4-$5");
-
-    }
-
+  if (ncpf <= 11) {
+    cpf_cnpj.value = cpf_cnpj.value.replace(
+      /^(\d{3})(\d{3})(\d{3})(\d{2})/,
+      "$1.$2.$3-$4"
+    );
+  } else if (ncpf > 11) {
+    cpf_cnpj.value = cpf_cnpj.value.replace(
+      /^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/,
+      "$1.$2.$3/$4-$5"
+    );
+  }
 }
 
 //MASCARA TELEFONE
 
-function mascara_fone(){
-    var fone = document.querySelector('#fone')
+function mascara_fone() {
+  var fone = document.querySelector("#fone");
 
-    fone.value = fone.value.replace(/^(\d{2})(\d{1})(\d{4})(\d{4})/, "($1) $2 $3-$4");
+  fone.value = fone.value.replace(
+    /^(\d{2})(\d{1})(\d{4})(\d{4})/,
+    "($1) $2 $3-$4"
+  );
 }
 
 //MASCARA DATA
 
-function mascara_data(){
-    var data = document.querySelector('#data')
+function mascara_data() {
+  var data = document.querySelector("#data");
 
-    data.value = data.value.replace(/^(\d{2})(\d{2})(\d{4})/, "$1/$2/$3");
-
+  data.value = data.value.replace(/^(\d{2})(\d{2})(\d{4})/, "$1/$2/$3");
 }
 
 //ESCONDER IMPUT OUTROS ENQUANTO OUTROS NÃO TIVER SELECIONADO NO SELECT MODELO
 
-function esconder(){
-    var modelo = document.querySelector('#modelo')
-    var outros = document.querySelector('#outros')
+function esconder() {
+  var outros = document.querySelector("#outros");
+  var modelo = document.querySelector("#modelo");
 
-    if(modelo.value === "Outros"){
-        outros.style.display = 'block';
-    }else{
-        outros.style.display = 'none';
-    }
+  if (modelo.value === "Outros") {
+    outros.style.display = "block";
+  }else{
+    outros.style.display = 'none';
+  }
 }
 
-esconder()
+
